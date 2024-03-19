02ee693c6bee36cbf65a7da38ef595f3c102728b
jemmert <jozemel5@gmail.com>
2024-03-19 17:18:51 -0300

    se maquetó y conectó al backend el login

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,10 +2,27 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { NavLink } from 'react-router-dom';
+import Login from './sections/Login';
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
 
 
 const NavBar = () => {
+
+  const [isOpen, setIsOpen]=useState(false);
+
+  const handleShow=()=>{
+    setIsOpen(true);
+  };
+
+  const handleClose=()=>{
+    setIsOpen(false);
+  };
+   
+
     return (
+       <>
+       <Login isOpen={isOpen} handleClose={handleClose}/>
         <Navbar expand="lg" className="bg-body-tertiary" data-bs-theme="dark" bg="dark">
         <Container>
           <Navbar.Brand href="#home">CRUD</Navbar.Brand>
@@ -19,9 +36,14 @@ const NavBar = () => {
               <NavLink to={'/acercadenosotros'} className={'nav-link'}>Acerca de Nosotros</NavLink>
               <NavLink to={'/administracion '} className={'nav-link'}>Administración</NavLink>
             </Nav>
+            <Nav>
+              <Button variant="primary" className="mx-2 my-2 my-md-0" onClick={handleShow} >Login</Button>
+              <Button variant="secondary" className="mx-2 my-2 my-md-0">Logout</Button>
+            </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
+       </>
     );
 };
 
---
+++ src/components/sections/Login.jsx
@@ -0,0 +1,123 @@
+import { Button, Modal, Form } from "react-bootstrap";
+import clsx from "clsx";
+import * as Yup from "yup";
+import { useFormik } from "formik";
+import axios from "axios";
+
+const Login = ({ isOpen, handleClose }) => {
+  const API=import.meta.env.VITE_APIV2;
+  const LoginSchema = Yup.object().shape({
+    email: Yup.string()
+      .email("Formato invalido")
+      .min(7)
+      .max(128)
+      .required("El email es requerido"),
+    password: Yup.string()
+      .min(6)
+      .max(20)
+      .required("La contraseña es requerido"),
+  });
+
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema: LoginSchema,
+    validateOnBlur: true,
+    validateOnChange: true,
+    onSubmit: async (values) => {
+      console.log("VALUES-->", values);
+      try {
+        const response=await axios.post(`${API}/users/login`,values);
+        console.log('Respuesta Login-->', response.data);
+        if (response.status===200) {
+          formik.resetForm();
+          handleClose();
+        }else{
+          alert('Ocurrió un error')
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  });
+
+  return (
+    <>
+      <Modal show={isOpen} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Login</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={formik.handleSubmit} >
+            <Form.Group className="mb-3" controlId="Email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Ingrese su email"
+                name="email"
+                {...formik.getFieldProps("email")}
+                className={clsx(
+                  "form-control",
+                  {
+                    "is-invalid": formik.touched.email && formik.errors.email,
+                  },
+                  {
+                    "is-valid": formik.touched.email && !formik.errors.email,
+                  }
+                )}
+              />
+              {formik.touched.email && formik.errors.email && (
+                <div className="mt-2 text-danger fw-bolder">
+                  <span role="alert">{formik.errors.email}</span>
+                </div>
+              )}
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="Password">
+              <Form.Label>Contraseña</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Ingrese su contraseña"
+                name="password"
+                {...formik.getFieldProps("password")}
+                className={clsx(
+                  "form-control",
+                  {
+                    "is-invalid":
+                      formik.touched.password && formik.errors.password,
+                  },
+                  {
+                    "is-valid":
+                      formik.touched.password && !formik.errors.password,
+                  }
+                )}
+              />
+              {formik.touched.password && formik.errors.password && (
+                <div className="mt-2 text-danger fw-bolder">
+                  <span role="alert">{formik.errors.password}</span>
+                </div>
+              )}
+            </Form.Group>
+            <div>
+              <Button variant="primary" type="submit" className="mx-2">
+                Ingresar
+              </Button>
+              <Button
+                variant="secondary"
+                onClick={handleClose}
+                className="mx-2"
+              >
+                Cerrar
+              </Button>
+            </div>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
+
+export default Login;
