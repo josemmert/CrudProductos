2afada2b151859f3830af277a081fd97408e2244
jemmert <jozemel5@gmail.com>
2024-03-21 18:30:10 -0300

    Se completó la integración con filtros

--- src/components/sections/ListadoProductos.jsx
+++ src/components/sections/ListadoProductos.jsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
-import { Table } from "react-bootstrap";
+import { Form, Table, FormGroup, FormLabel, Row, Col } from "react-bootstrap";
 import Producto from "./Producto";
 import ModalEditar from "./ModalEditar";
 
 const ListadoProductos = () => {
   const [productos, setProductos] = useState([]);
-
   const [show, setShow] = useState(false);
   const [productEdit, setProductEdit] = useState(undefined);
+  const [filtroProducto, setFiltroProducto] = useState("");
+  const [busquedaTitulo, setBusquedaTitulo]= useState("");
 
   const handleClose = () => {
     setProductEdit(undefined);
@@ -27,7 +28,15 @@ const ListadoProductos = () => {
 
   const getProductos = async () => {
     try {
-      const response = await fetch(`${API}/products`);
+      let URL = `${API}/products`;
+      if (filtroProducto !== "" && busquedaTitulo==="") {
+        URL = `${API}/products?filtro=${filtroProducto}`;
+      }else if (filtroProducto !== "" && busquedaTitulo!=="") {
+        URL = `${API}/products?filtro=${filtroProducto}&busqueda=${busquedaTitulo}`;
+      }else if (filtroProducto === "" && busquedaTitulo!=="") {
+        URL = `${API}/products?busqueda=${busquedaTitulo}`;
+      }
+      const response = await fetch(URL);
       //console.log('RESPONSE-->', response);
       const resJson = await response.json();
       //console.log('RESJSON', resJson);
@@ -37,13 +46,19 @@ const ListadoProductos = () => {
     }
   };
 
+  // useEffect(() => {
+  //   getProductos();
+
+  //   return () => {
+  //     setProductos([]);
+  //   };
+  // }, []);
+
   useEffect(() => {
     getProductos();
+  }, [filtroProducto,busquedaTitulo]);
 
-    return () => {
-      setProductos([]);
-    };
-  }, []);
+  //console.log("FiltroBusqueda", busquedaTitulo);
 
   return (
     <>
@@ -56,6 +71,46 @@ const ListadoProductos = () => {
       <div className="container-fluid">
         <div className="text-center">
           <h2>Listado Productos</h2>
+        </div>
+        <div className="container-fluid">
+          <Row>
+            <Col xs={12} md={6}>
+              <Form>
+                <FormGroup className="mb-3" controlId="category">
+                  <FormLabel>Filtrar por categoría</FormLabel>
+                  <Form.Select
+                    aria-label="category"
+                    name="category"
+                    onChange={(e) => {
+                      setFiltroProducto(e.currentTarget.value);
+                    }}
+                  >
+                    <option value="">Todas</option>
+                    <option value="Bebidas">Bebidas</option>
+                    <option value="Alimentos">Alimentos</option>
+                    <option value="Limpieza">Limpieza</option>
+                  </Form.Select>
+                </FormGroup>
+              </Form>
+            </Col>
+            <Col xs={12} md={6}>
+              <Form onSubmit={(e)=>{
+                e.preventDefault();
+              }}>
+                <Form.Group controlId="busqueda">
+                  <Form.Label>Buscar por Titulo</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Ingrese el titulo del producto"
+                    name="title"
+                    onChange={(e) => {
+                      setBusquedaTitulo(e.currentTarget.value);
+                    }}
+                  />
+                </Form.Group>
+              </Form>
+            </Col>
+          </Row>
         </div>
         <div className="table-responsive">
           <Table striped bordered hover variant="dark">
